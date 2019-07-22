ffa66a82b04a141f1a16b040f9720a56036ff8de
ErikBoesen <me@erikboesen.com>
2019-07-21 21:14:33 -0400

    Fix example naming

--- example.py
+++ example.py
@@ -2,11 +2,12 @@ import yalegatewayservicemetrics
 import os
 from pprint import pprint
 
-# "api" name can be whatever is most convenient for your program
+# 'api' name can be whatever is most convenient for your program
 api = yalegatewayservicemetrics.API(os.environ['YALE_API_KEY'])
 
-print("Converting URL to service name: " + api.service_name('/soa-gateway/energy/data'))
-print("Converting name back to URL: " + api.request_url('EnergyData'))
-print("Getting a full URL from just the path: " + api.endpoint('EnergyData'))
-print("Getting actual records:")
-print(api.service('Laundry-school').records())
+print('Getting summary records:')
+print(api.service('Laundry-school').summary())
+print('Let\'s start with a few helper methods...')
+print('Converting URL to service name: ' + api.service_name('/soa-gateway/energy/data'))
+print('Converting name back to URL: ' + api.request_url('EnergyData'))
+print('Getting a full URL from just the path: ' + api.endpoint('EnergyData'))
