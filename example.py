4458395f6c21d586ea4b7d2a5554641fc48c0af6
ErikBoesen <me@erikboesen.com>
2019-07-21 20:01:53 -0400

    Fix example

--- example.py
+++ example.py
@@ -5,10 +5,8 @@ from pprint import pprint
 # "api" name can be whatever is most convenient for your program
 api = yalegatewayservicemetrics.API(os.environ['YALE_API_KEY'])
 
-"""
-print(api.service_name('/soa-gateway/energy/data'))
-print(api.request_url('EnergyData'))
-print(api.endpoint('EnergyData'))
-pprint(api.summary('Laundry-school'))
-"""
+print("Converting URL to service name: " + api.service_name('/soa-gateway/energy/data'))
+print("Converting name back to URL: " + api.request_url('EnergyData'))
+print("Getting a full URL from just the path: " + api.endpoint('EnergyData'))
+print("Getting actual records:")
 print(api.service('Laundry-school').records())
